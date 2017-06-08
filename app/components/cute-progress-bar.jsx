6dbbc1685e9288b9f40e63fbca595b8ec918cece
Mindjet <pearl920@outlook.com>
2017-06-08 18:29:41 +0800

    Update: add drag motion to CuteProgressBar and handle edge movement

--- app/components/cute-progress-bar.jsx
+++ app/components/cute-progress-bar.jsx
@@ -1,6 +1,7 @@
 'use strict';
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 import '../css/cute-progress-bar.css';
 
 let thumbColor = '#146365';
@@ -15,9 +16,11 @@ let progressed = null;
 let progressWhole = null;
 let progressBackground = null;
 
+let containerWidth;
+let containerLeft;
 let progressWholeLength;
 let progressedLength;
-let thumbRadius;
+let thumbWidth;
 let offset;
 
 let moving = false;
@@ -36,35 +39,47 @@ class CuteProgressBar extends React.Component {
         progressBackground = this.refs.cpbProgressBackground;
 
         thumb.style.backgroundColor = thumbColor;
+        thumb.style.cursor = 'pointer';
         progressed.style.backgroundColor = progressBackgroundColor;
 
         this.doMeasurement();
-        this.initThumbDrag();
+        this.initDragEvent();
     }
 
     doMeasurement() {
-        progressWholeLength = progressWhole.style.offsetWidth;
-        thumbRadius = thumb.style.offsetWidth;
+        containerWidth = container.offsetWidth;
+        containerLeft = container.offsetLeft;
+        progressWholeLength = progressWhole.offsetWidth;
+        thumbWidth = thumb.offsetWidth;
+        console.log('container width: ' + containerWidth);
+        console.log('progressWholeLength: ' + progressWholeLength);
+        console.log('thumb width: ' + thumbWidth);
     }
 
-    initThumbDrag() {
-        thumb.onmousedown = (e) => {
-            //todo record offset
-            moving = true;
-            console.log("on mouse down");
-        };
-
-        thumb.onmousemove = (e) => {
-            console.log(e);
-            thumb.style.left = e.offsetX + 'px';
-        };
-
-
-        thumb.onmouseup = (e) => {
-            moving = false;
-            console.log('on mouse up');
-        };
+    initDragEvent() {
+        document.addEventListener('mousedown', (e) => this.startDrag(e), false);
+        document.addEventListener('mousemove', (e) => this.onDragging(e), false);
+        document.addEventListener('mouseup', (e) => this.endDrag(e), false);
     }
+
+    startDrag(e) {
+        moving = true;
+        offset = e.offsetX;
+    }
+
+    onDragging(e) {
+        if (!moving) return;
+        //edge detection.
+        let thumbLeft = e.screenX - offset;
+        thumbLeft = thumbLeft <= containerLeft ? containerLeft : thumbLeft;
+        thumbLeft = thumbLeft >= containerWidth - thumbWidth ? containerWidth - thumbWidth : thumbLeft;
+        thumb.style.left = thumbLeft + 'px';
+    }
+
+    endDrag(e) {
+        moving = false;
+    }
+
 
     render() {
         return (
